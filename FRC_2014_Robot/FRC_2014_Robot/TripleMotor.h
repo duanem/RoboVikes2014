c07da519cd99fc87571159801ff3fa612073b67b
aaronistheman <aarons.7007@gmail.com>
2014-01-24 15:12:27 -0800

    Added TripleMotor, shift code, and tank drive code.

---
+++ FRC_2014_Robot/FRC_2014_Robot/TripleMotor.h
@@ -0,0 +1,27 @@
+#ifndef TRIPLE_MOTOR_H_
+#define TRIPLE_MOTOR_H_
+
+#include "SpeedController.h"
+class TripleMotor: public SpeedController
+{
+public:
+	TripleMotor(SpeedController* speedController1,
+				SpeedController* speedController2,
+                                SpeedController* speedController3);
+	
+	SpeedController* GetSpeedController1();	
+	SpeedController* GetSpeedController2();
+        SpeedController* GetSpeedController3();	
+	virtual void Set(float speed, UINT8 syncGroup);
+	virtual float Get();
+	
+	virtual void Disable();
+	
+	virtual void PIDWrite(float output);
+protected:
+	SpeedController* mSpeedController1;
+	SpeedController* mSpeedController2;
+        SpeedController* mSpeedController3;
+};
+
+#endif
