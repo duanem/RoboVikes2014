fa5356247cc5987c8562fbf8c0d60d0697d0a74a
andrewjpiro <andrewjpiro@gmail.com>
2014-02-15 08:15:38 -0800

    Added DoubleMotor

---
+++ FRC_2014_Robot/FRC_2014_Robot/DoubleMotor.cpp
@@ -0,0 +1,37 @@
+
+#include "DoubleMotor.h"
+
+DoubleMotor::DoubleMotor(SpeedController* speedController1,
+			SpeedController* speedController2)
+:mSpeedController1(speedController1),
+ mSpeedController2(speedController2)
+{}
+
+SpeedController* DoubleMotor::GetSpeedController1()
+{return mSpeedController1;}
+
+SpeedController* DoubleMotor::GetSpeedController2()
+{return mSpeedController2;}
+
+void DoubleMotor::Set(float speed, UINT8 syncGroup)
+{
+	mSpeedController1->Set(speed, syncGroup);
+	mSpeedController2->Set(speed, syncGroup);
+}
+
+float DoubleMotor::Get()
+{
+	return mSpeedController1->Get();
+}
+void DoubleMotor::Disable()
+{
+	mSpeedController1->Disable();
+	mSpeedController2->Disable();
+}
+
+void DoubleMotor::PIDWrite(float output)
+{
+	 Set(output);
+}
+
+
